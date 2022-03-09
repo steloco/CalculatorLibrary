af41dbb54dc258ebe5ba7c8eca60e6a8676ff824
Steven Locorotondo <steven.locorotondo@railsbank.com>
2022-03-09 15:36:47 +0000

    reformat

--- calculator.py
+++ calculator.py
@@ -11,5 +11,6 @@ def subtract(first_term, second_term):
     return first_term - second_term
 
 
-def multiply(first_term, second_term):
+def multiply(
+        first_term, second_term):
     return first_term * second_term
